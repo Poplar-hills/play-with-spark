da807b8cf7dadf5cabe8a91aefbbeb384a6337bd
Max Jiang <maxjiang23326@gmail.com>
2019-06-06 16:04:14 +0800

    feat: add basic DataFrame operations

--- src/main/scala/sparkTutorial/dataframe/DataFrameApp.scala
+++ src/main/scala/sparkTutorial/dataframe/DataFrameApp.scala
@@ -4,7 +4,42 @@ import org.apache.spark.sql.SparkSession
 
 object DataFrameApp {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("DataFrameApp").master("local[2]").getOrCreate()
-    spark.read.format("json").load("")
+    val spark = SparkSession.builder()
+      .appName("DataFrameApp")
+      .master("local[2]")
+      .getOrCreate()
+
+    val ordersDF = spark.read
+      .option("multiline", value = true)
+      .json("file:///Users/myjiang/Documents/Poplar_hills/Dev/spark/play-with-spark/in/orders.json")
+
+    // 输出 DataFrame 的 schema 信息
+    ordersDF.printSchema()
+
+    // 查询所有列的数据（默认前20条）
+    ordersDF.show()
+
+    // 查询某一列的数据：SELECT Quantity FROM table
+    ordersDF.select("Quantity").show()
+
+    // 查询某几列的数据：SELECT OrderId, Quantity*10 as Quantity2 FROM table
+    ordersDF.select(
+      ordersDF.col("OrderId"),
+      (ordersDF.col("Quantity") * 10).as("Quantity2")  // 变换该列数据，并重命名列名
+    ).show()
+
+    // 过滤：SELECT OrderId, Quantity FROM table WHERE Quantity > 3
+    ordersDF
+      .filter(ordersDF.col("Quantity") > 3)  // .filter() 和 .where() 等效
+      .select("OrderId", "Quantity")  // 上面查询某几列的简化写法（但不能变换）
+      .show()
+
+    // 聚合：SELECT OrderId, COUNT(1) FROM table GROUP BY Quantity
+    ordersDF
+      .groupBy("Quantity")
+      .count()
+      .show()
+
+    spark.stop()
   }
 }
